d58f7fd2dbf4ce7db007f69a5b371c720024d51d
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-06-03 15:11:31 +0200

    fix some dysfunctionality in insert basics

--- FileStructuresLab1/src/eg/edu/alexu/csd/filestructure/redblacktreeImplementation/RedBlackTree.java
+++ FileStructuresLab1/src/eg/edu/alexu/csd/filestructure/redblacktreeImplementation/RedBlackTree.java
@@ -6,6 +6,7 @@ import eg.edu.alexu.csd.filestructure.redblacktree.IRedBlackTree;
 public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T, V> {
 
 	private Node<T,V> nil =new Node<T,V>();
+	private Node<T,V> nilLeaf =new Node<T,V>();
 	private Node<T,V> root=nil;
 	@Override
 	public INode<T, V> getRoot() {
@@ -31,8 +32,8 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 		// TODO Auto-generated method stub
 		Node<T,V> temp =new Node<T,V>(root.getKey(),root.getValue(),root.getColor(),root.getRightChild(),root.getLeftChild());
 		while(!temp.isNull()) {
-			if(root.getKey().compareTo(key)==0)
-				return root.getValue();
+			if(temp.getKey().compareTo(key)==0)
+				return temp.getValue();
 			else if(temp.getKey().compareTo(key)<0) {
 			temp=(Node<T, V>) temp.getRightChild();
 			}
@@ -52,13 +53,15 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 	@Override
 	public void insert(T key, V value) {
 		// TODO Auto-generated method stub
-		if(root.isNull()) {//inserting in empty tree
-			root=new Node<T,V>(key,value,Node.BLACK,nil,nil);
+ 		if(root.isNull()) {//inserting in empty tree
+			root=new Node<T,V>(key,value,Node.BLACK,nilLeaf,nilLeaf);
 			root.setParent(nil);
+			nilLeaf.setParent(root);
 			return;
 		}
-		Node<T,V> temp =new Node<T,V>(root.getKey(),root.getValue(),root.getColor(),root.getRightChild(),root.getLeftChild());
-		while(!temp.isNull()) {
+		//Node<T,V> temp =new Node<T,V>(root.getKey(),root.getValue(),root.getColor(),root.getRightChild(),root.getLeftChild());
+ 		Node<T,V> temp=root;
+ 		while(!temp.isNull()) {
 			if(temp.getKey().compareTo(key)<0) {
 				temp=(Node<T, V>) temp.getRightChild();
 			}
@@ -69,15 +72,29 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 				break;//the same key so i update the associated node
 		}
 		Node <T,V>parent= (Node<T, V>) temp.getParent();
-		temp=new Node<T,V>(key,value,Node.RED,nil,nil);
+		temp.setColor(Node.RED);
+		temp.setKey(key);
+		temp.setValue(value);
+		temp.setRightChild(nilLeaf);
+		temp.setLeftChild(nilLeaf);
 		temp.setParent(parent);
+		
 		//call the fix up
+		//insertFix()
 	}
 
 	@Override
 	public boolean delete(T key) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	//<------------------------------------------------------------------------------------------------------------------------------------>
+	
+	/*takes the new inserted node and checks if there is a 
+	 * violation and fix it depending on the current violation*/
+	private void insertFix(Node<T,V> inserted) {
+		
 	}
 	private void rotateRight(Node<T,V> toBeRotated) {
 		
@@ -97,7 +114,7 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 		temp.setRightChild(toBeRotated);
 		toBeRotated.setParent(temp);
 	}
-private void rotateLeft(Node<T,V> toBeRotated) {
+	private void rotateLeft(Node<T,V> toBeRotated) {
 		
 		Node <T,V> temp=(Node<T, V>) toBeRotated.getRightChild();
 		toBeRotated.setRightChild(temp.getLeftChild());
