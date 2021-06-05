9af4ad82d528305a794402e03e27a4d84f7e9cb9
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-06-05 03:28:27 +0200

    fixing some problems

--- FileStructuresLab1/src/eg/edu/alexu/csd/filestructure/redblacktreeImplementation/RedBlackTree.java
+++ FileStructuresLab1/src/eg/edu/alexu/csd/filestructure/redblacktreeImplementation/RedBlackTree.java
@@ -5,9 +5,9 @@ import eg.edu.alexu.csd.filestructure.redblacktree.IRedBlackTree;
 
 public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T, V> {
 
-	private Node<T,V> nil =new Node<T,V>();
+	private Node<T,V> nil = new Node<T,V>();
 	//private Node<T,V> nilLeaf =new Node<T,V>();
-	private Node<T,V> root=nil;
+	private Node<T,V> root = nil;
 	@Override
 	public INode<T, V> getRoot() {
 		// TODO Auto-generated method stub
@@ -24,21 +24,21 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		this.root=nil;
+		this.root = nil;
 	}
 
 	@Override
-	public V search(T key) {
+	public V search(T comparable) {
 		// TODO Auto-generated method stub
 		Node<T,V> temp =new Node<T,V>(root.getKey(),root.getValue(),root.getColor(),root.getRightChild(),root.getLeftChild());
 		while(!temp.isNull()) {
-			if(temp.getKey().compareTo(key)==0)
+			if(temp.getKey().compareTo(comparable)==0)
 				return temp.getValue();
-			else if(temp.getKey().compareTo(key)<0) {
-			temp=(Node<T, V>) temp.getRightChild();
+			else if(temp.getKey().compareTo(comparable)<0) {
+			temp = (Node<T, V>) temp.getRightChild();
 			}
 			else {
-			temp=(Node<T, V>) temp.getLeftChild();
+			temp = (Node<T, V>) temp.getLeftChild();
 			}
 		}
 		return null;
@@ -85,17 +85,144 @@ public class RedBlackTree<T extends Comparable<T>, V> implements IRedBlackTree<T
 			parent.setLeftChild(holder);
 		else
 			parent.setRightChild(holder);
-		
-		
 		//call the fix up
 		insertFix(holder);
 	}
 
+	//<------------------------------------------------------------------------------------------------------------------------------------>	
+	
 	@Override
 	public boolean delete(T key) {
 		// TODO Auto-generated method stub
-		return false;
+		if(search(key) == null) {  // if the tree doesn't contain the node to be deleted
+			return false;
+		}else {
+			removeHelper(key);
+			return true;
+		}
 	}
+	
+
+	private void removeHelper(T key){
+		Node z = searchDelete(key);
+		Node x = new Node<>(); //nil
+		Node y = new Node<>(); //nil
+
+		if (z.getLeftChild().isNull() || z.getRightChild().isNull())
+			y = z;
+		else y = treeSuccessor(z);
+		if (!y.getLeftChild().isNull()) {
+			x = (Node) y.getLeftChild();
+		}else {
+			x = (Node)y.getRightChild();
+		}
+		x.setParent(y.getParent());
+		if (y.getParent().isNull()) {
+			root = x;
+		}else if (!y.getParent().getLeftChild().isNull() && compareTwoNodes(y.getParent().getLeftChild(),y)) {
+			y.getParent().setLeftChild(x);
+		}else if (!y.getParent().getRightChild().isNull() && compareTwoNodes(y.getParent().getRightChild(),y)) {
+			y.getParent().setRightChild(x);
+		}
+		if (!compareTwoNodes(y, z)){
+			z.setKey(y.getKey());
+		}
+		if (y.getColor() == Node.BLACK)
+			removeFixup(x);
+	}
+
+	private Node treeSuccessor(Node x){
+		if (!x.getLeftChild().isNull()) {
+			return treeMinimum((Node) x.getRightChild());
+		}
+		Node result = (Node) x.getParent();
+		while (!result.isNull() && x == result.getRightChild()){
+			x = result;
+			result = (Node) result.getParent();
+		}
+		return result;
+	}
+	
+	private Node treeMinimum(Node node){
+		while (!node.getLeftChild().isNull()) {
+			node = (Node) node.getLeftChild();
+		}
+		return node;
+	}
+
+	private void removeFixup(Node x){
+		Node w;
+		while (!compareTwoNodes(x, root) && x.getColor() == Node.BLACK){
+			if (compareTwoNodes(x, x.getParent().getLeftChild())){
+				w = (Node) x.getParent().getRightChild();
+				if (w.getColor() == Node.RED){
+					w.setColor(Node.BLACK);
+					x.getParent().setColor(Node.RED);
+					rotateLeft((Node<T, V>) x.getParent());
+					w = (Node) x.getParent().getRightChild();
+				}
+				if (w.getLeftChild().getColor() == Node.BLACK &&
+							w.getRightChild().getColor() == Node.BLACK){
+					w.setColor(Node.RED);
+					x = (Node) x.getParent();
+				}else{
+					if (w.getRightChild().getColor() == Node.BLACK){
+						w.getLeftChild().setColor(Node.BLACK);
+						w.setColor(Node.RED);
+						rotateRight(w);
+						w = (Node) x.getParent().getRightChild();
+					}
+					w.setColor(x.getParent().getColor());
+					x.getParent().setColor(Node.BLACK);
+					w.getRightChild().setColor(Node.BLACK);
+					rotateLeft((Node<T, V>) x.getParent());
+					x = root;
+				}
+			}else{
+				w = (Node) x.getParent().getLeftChild();
+				if (w.getColor() == Node.RED){
+					w.setColor(Node.BLACK);
+					x.getParent().setColor(Node.RED);
+					rotateRight((Node) x.getParent());
+					w = (Node) x.getParent().getLeftChild();
+				}
+				if (w.getRightChild().getColor() == Node.BLACK &&
+							w.getLeftChild().getColor() == Node.BLACK){
+					w.setColor(Node.RED);
+					x = (Node) x.getParent();
+				}else{
+					 if (w.getLeftChild().getColor() == Node.BLACK){
+						w.getRightChild().setColor(Node.BLACK);
+						w.setColor(Node.RED);
+						rotateLeft(w);
+						w = (Node) x.getParent().getLeftChild();
+					}
+					w.setColor(x.getParent().getColor());
+					x.getParent().setColor(Node.BLACK);
+					w.getLeftChild().setColor(Node.BLACK);
+					rotateRight((Node<T, V>) x.getParent());
+					x = root;
+				}
+			}
+		}
+		x.setColor(Node.BLACK);
+	}
+	
+	public Node searchDelete(T key) {
+		Node<T,V> temp =new Node<T,V>(root.getKey(),root.getValue(),root.getColor(),root.getRightChild(),root.getLeftChild());
+		while(!temp.isNull()) {
+			if(temp.getKey().compareTo(key) == 0) {
+				return temp;
+			}else if(temp.getKey().compareTo(key)<0) {
+				temp = (Node<T, V>) temp.getRightChild();
+			}else {
+				temp = (Node<T, V>) temp.getLeftChild();
+			}
+		}
+		return null;
+	}
+		
+	
 	
 	//<------------------------------------------------------------------------------------------------------------------------------------>
 	
